872b736e28c95cca8021240acaee506d43909f25
Violacordis <ikemviolacordis@gmail.com>
2023-03-10 13:38:12 +0100

    socket.io connection

--- public/index.js
+++ public/index.js
@@ -1,6 +1,37 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+
 const socket = io();
 
 // Message from server
 socket.on('message', message => {
-    console.log(message);
+    outputMessage(message);
+
+    // Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 })
+
+// Message submit
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Get text from input using the id of the input field (msg)
+    let text = e.target.elements.msg.value;
+    
+    // Emit message to server
+    socket.emit('chatMessage', text);
+
+    // clear text input field after message is sent
+    e.target.elements.msg.value = '';
+
+    // Focus on text input field after message is sent and the field cleared
+    e.target.elements.msg.focus();
+});
+
+// Output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="text"> ${message} </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+  }
--- server.js
+++ server.js
@@ -11,12 +11,17 @@ const io = socketio(server);
 const PORT = process.env.PORT;
 
 // Setting up static files
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when client connects
 io.on('connection', socket => {
     // Welcome current user
-    socket.emit('message', 'Welcome! Great to have you here. My name is ChatBot. I am here to help you. \n Select 1 to Place an order \n Select 99 to checkout order \n Select 98 to see order history \n Select 97 to see current order \n Select 0 to cancel order');
+    socket.emit('message', 'Welcome! Great to have you here. My name is ChatBot. I am here to help you.\nBelow are the options to select from:\n\nSelect 1 to place an order\nSelect 99 to checkout order\nSelect 98 to see order history\nSelect 97 to see current order\nSelect 0 to cancel order');
+
+    // listen for chatMessage
+    socket.on('chatMessage', text => {
+        io.emit('message', text);
+    })
 })
 
 server.listen(PORT, () => {
